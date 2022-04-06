9c6a8b15129783523875e581238011ba2a4d7827
JuanCarlosMtz <A01612967@itesm.mx>
2022-04-06 12:01:05 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home';
+import Game from './pages/Game';
+import Ranking from './pages/Ranking';
+import Closet from './pages/Closet';
+import Contents from './pages/Contents';
+import Store from './pages/Store';
+import AboutTWI from './pages/AboutTWI';
+import LogPage from './pages/LogPage';
+import HomeA from './pages/HomeA';
+import AddToStore from './pages/AddToStore';
+import AlterQuizzes from './pages/AlterQuizzes';
+import Analytics from './pages/Analytics';
+import RankingA from './pages/RankingA';
+import ViewDocuments from './pages/ViewDocuments';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Routes>
+        <Route path="/" element={<LogPage />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/homea' element={<HomeA />} />
+        <Route path='/analytics' element={<Analytics />} />
+        <Route path='/game' element={<Game />} />
+        <Route path='/ranking' element={<Ranking />} />
+        <Route path='/rankinga' element={<RankingA />} />
+        <Route path='/closet' element={<Closet />} />
+        <Route path='/contents' element={<Contents />} />
+        <Route path='/viewdocuments' element={<ViewDocuments />} />
+        <Route path='/store' element={<Store />} />
+        <Route path='/addtostore' element={<AddToStore />} />
+        <Route path='/alterquizzes' element={<AlterQuizzes />} />
+        <Route path='/aboutTWI' element={<AboutTWI />} />
+        </Routes>
+      </Router>
     </div>
   );
 }

3d5c46ff42b8bb4efcd45e54aa42ad71b3b3e158
JuanCarlosMtz <A01612967@itesm.mx>
2022-04-06 18:31:57 -0500

    main

--- src/components/NavBarA.js
+++ src/components/NavBarA.js
@@ -1,38 +1,62 @@
-import React, { Component } from 'react';
-import TWIBlack from '../logos/twiBlack.png';
-import UserIcon from '../icons/userIconBlack.png';
-import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+import React, { Component } from "react";
+import TWIBlack from "../logos/twiBlack.png";
+import UserIcon from "../icons/userIconBlack.png";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
- 
 
 export default class NavBarU extends Component {
-    render() {
-        return (
-                <div>
-                <Navbar bg="light" expand="lg">
-                    <Container>
-                        <Navbar.Brand as={Link} to={"/homea"}><img src={TWIBlack} width="120" height="45" className="d-inline-block align-top" alt="TWIRed logo" /></Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="ms-auto">
-                                <Nav.Link as={Link} to={"/rankinga"}>Ranking</Nav.Link>
-                                <Nav.Link as={Link} to={"/analytics"}>Analíticos</Nav.Link>
-                                <Nav.Link as={Link} to={"/viewdocuments"}>Revisar</Nav.Link>
-                                <Nav.Link as={Link} to={"/alterquizzes"}>Quizzes</Nav.Link>
-                                <Nav.Link as={Link} to={"/addtostore"}>Tienda</Nav.Link>
-                            </Nav>
-                            <Nav className="ms-auto">
-                                <img src={UserIcon} width="45" height="45" className="d-inline-block align-top" alt="UserBlack logo" />
-                                <NavDropdown title="Administrador" id="basic-nav-dropdown">
-                                    <NavDropdown.Item as={Link} to={"/"}>Cerrar Sesión</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                                </NavDropdown>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-                </div>
-        )
-    }
-};
+  render() {
+    return (
+      <div>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand as={Link} to={"/homea"}>
+              <img
+                src={TWIBlack}
+                width="120"
+                height="45"
+                className="d-inline-block align-top"
+                alt="TWIRed logo"
+              />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link as={Link} to={"/rankinga"}>
+                  Ranking
+                </Nav.Link>
+                <Nav.Link as={Link} to={"/analytics"}>
+                  Analíticos
+                </Nav.Link>
+                <Nav.Link as={Link} to={"/viewdocuments"}>
+                  Revisar
+                </Nav.Link>
+                <Nav.Link as={Link} to={"/alterquizzes"}>
+                  Quizzes
+                </Nav.Link>
+              </Nav>
+              <Nav className="ms-auto">
+                <img
+                  src={UserIcon}
+                  width="45"
+                  height="45"
+                  className="d-inline-block align-top"
+                  alt="UserBlack logo"
+                />
+                <NavDropdown title="Administrador" id="basic-nav-dropdown">
+                  <NavDropdown.Item as={Link} to={"/"}>
+                    Cerrar Sesión
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
--- src/components/RankingAContent.js
+++ src/components/RankingAContent.js
@@ -1,92 +1,111 @@
-import React, { Component } from 'react';
-import './RankingAContent.css';
-import User from '../img/stickMan.png';
-import pos1 from '../img/tier1.png';
-import pos2 from '../img/tier2.png';
-import pos3 from '../img/tier3.png';
-import { Row, Col, Table } from 'react-bootstrap';
+import React from "react";
+import "./RankingAContent.css";
+import User from "../img/stickMan.png";
+import pos1 from "../img/tier1.png";
+import pos2 from "../img/tier2.png";
+import pos3 from "../img/tier3.png";
+import { Row, Col, Table } from "react-bootstrap";
+import { useState, useEffect } from "react";
 
-export default class RankingAContent extends Component {
-    render() {
-        return (
-            <div>
-                <Row className="avatares">
-                    <Col sm={4}>
-                        <Row>
-                            <h4>[Usuario2]</h4>
-                                <div className="user2">
-                                    <img ClassName="img-fluid" className="mx-auto" src={User} />
-                                </div>
-                        </Row>
-                        <Row>
-                            <div className="pos2">
-                                <img ClassName="img-fluid" className="mx-auto" src={pos2} />
-                            </div>
-                        </Row>
-                    </Col>
-                    <Col sm={4}>
-                        <Row>
-                            <h4>[Usuario1]</h4>
-                            <div className="user1">
-                                <img ClassName="img-fluid" className="mx-auto" src={User} />
-                            </div>
-                        </Row>
-                        <Row>
-                            <div className="pos1">
-                                <img ClassName="img-fluid" className="mx-auto" src={pos1} />
-                            </div>
-                        </Row>
-                    </Col>
-                    <Col sm={4}>
-                        <Row>
-                            <h4>[Usuario3]</h4>
-                            <div className="user3">
-                                <img ClassName="img-fluid" className="mx-auto" src={User} />
-                            </div>
-                        </Row>
-                        <Row>
-                            <div className="pos3">
-                                <img ClassName="img-fluid" className="mx-auto" src={pos3} />
-                            </div>
-                        </Row>
-                    </Col>
-                </Row>
-                        <Row>
-                            <div className="tabla">
-                                <Table responsive striped bordered hover variant="dark">
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>First Name</th>
-                                            <th>Last Name</th>
-                                            <th>Username</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>Mark</td>
-                                            <td>Otto</td>
-                                            <td>@mdo</td>
-                                        </tr>
-                                        <tr>
-                                            <td>2</td>
-                                            <td>Jacob</td>
-                                            <td>Thornton</td>
-                                            <td>@fat</td>
-                                        </tr>
-                                        <tr>
-                                            <td>3</td>
-                                            <td>Larry</td>
-                                            <td>the Bird</td>
-                                            <td>@twitter</td>
-                                        </tr>  
-                                    </tbody>
-                                </Table>
-                            </div>
-                        </Row>
+function RankingAContent() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/ranking`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        return response.json();
+      })
+      .then((actualData) => {
+        setData(actualData);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setData(null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Ranking</h1>
+      {loading && <div>A moment please...</div>}
+      {error && (
+        <div>{`There is a problem fetching the post data - ${error}`}</div>
+      )}
+      <Row className="avatares mx-auto">
+        <Col sm={4}>
+          <Row>
+            <h4>[Usuario2]</h4>
+            <div className="user2">
+              <img ClassName="img-fluid" className="mx-auto" src={User} />
             </div>
-        )
-    }
+          </Row>
+          <Row>
+            <div className="pos2">
+              <img ClassName="img-fluid" className="mx-auto" src={pos2} />
+            </div>
+          </Row>
+        </Col>
+        <Col sm={4}>
+          <Row>
+            <h4>[Usuario1]</h4>
+            <div className="user1">
+              <img ClassName="img-fluid" className="mx-auto" src={User} />
+            </div>
+          </Row>
+          <Row>
+            <div className="pos1">
+              <img ClassName="img-fluid" className="mx-auto" src={pos1} />
+            </div>
+          </Row>
+        </Col>
+        <Col sm={4}>
+          <Row>
+            <h4>[Usuario3]</h4>
+            <div className="user3">
+              <img ClassName="img-fluid" className="mx-auto" src={User} />
+            </div>
+          </Row>
+          <Row>
+            <div className="pos3">
+              <img ClassName="img-fluid" className="mx-auto" src={pos3} />
+            </div>
+          </Row>
+        </Col>
+      </Row>
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Puntaje</th>
+          </tr>
+        </thead>
+        {data &&
+          data.ranking.map(({ idranking, nombre, apellido, puntaje }) => (
+            <tbody>
+              <tr>
+                <td>{idranking}</td>
+                <td>{nombre}</td>
+                <td>{apellido}</td>
+                <td>{puntaje}</td>
+              </tr>
+            </tbody>
+          ))}
+      </Table>
+    </div>
+  );
 }
-        
+
+export default RankingAContent;
